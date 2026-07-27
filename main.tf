cad3fc06074d44af5a026b72efa2e907ee162582
Anjali Amar Prasad <rtr.anjali9819@gmail.com>
2026-07-27 16:54:11 +0530

    Add initial Terraform configuration for AWS
    Added initial Terraform configuration for AWS provider and EC2 instance.

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+############First Terraform code###########
+
 provider "aws" {
   region = "ap-south-1"
  }
+ 
+#First we need to specify Provider and the region where the resource needs to be created ##
+
+#Then what resource we need & the sub-configuration##
+
+resource "aws_instance" "web" {
+  ami = "ami-5667900"
+  instance_type = "t2.micro"
+  key_name = "my-name"
+}
+
+  tags = {
+    Name = "leran-terraform-1"
+}
