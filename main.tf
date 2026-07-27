c813db2b583574cfe9ca461417638cf1d47c9875
Anjali Amar Prasad <rtr.anjali9819@gmail.com>
2026-07-27 16:48:22 +0530

    Refactor AWS EC2 instance configuration
    Removed old EC2 instance configuration and variable definition. Added AWS provider configuration.

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-resource "aws_instance" "web" {
-  ami = "ami_test"
-  instance_type = "t3.small"
-  description = "Ec2 Instance"
-}
-
-variable "instance_type" {
-  type = string
-  description = "EC2 instance_type"
-  default = t2.small"
-}
-
-resource "aws_instance" "web" {
-  ami = data.ami_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = var.subnet_id
-}
-  tags = {
-    Environment = var.environment
-	Name = "${var.environment}-web-server"
-}
+provider "aws" {
+  region = "ap-south-1"
+ }
